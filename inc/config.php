5ea1f3401c145b388303048e7343b7dce73747e4
Claudio Morales <38842608+climoralesg@users.noreply.github.com>
2021-10-08 00:46:36 -0300

    feature: Agregada configuracion BD inicial

---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+    define ("DB_SERVER","127.0.0.1");
+    define ("USERNAME","root");
+    define ("DB_PASSWORD","");
+    define ("DB_DATABASE_NAME","division_politico_administrativa");
+?>
